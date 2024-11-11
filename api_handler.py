121f482bff066951f5114336062240eff85ddadb
Roopesh S <sroopesh6@gmail.com>
2024-11-11 23:58:07 +0530

    created

---
+++ api_handler.py
@@ -0,0 +1,171 @@
+# api_handler.py
+import requests # type: ignore
+import constants as constant
+
+# API 1: Get token
+def get_token():
+    url = constant.TOKEN_URL
+    # data = {'userName': username, 'password': password}
+    # print(data)
+    headers = {
+        'Content-Type':'application/x-www-form-urlencoded'
+        }
+    body = {
+        'client_id':'8GqRlWRMZFhfJ0R3XKCrG0jx9NQ6B3ho',
+        'grant_type':'password',
+        'username':'tsttch1@schstage264.com',
+        'password':'Test@123',
+        'isMobile':'true'
+    }
+    response = requests.post(url=url, headers=headers, json=body)
+    if response.status_code == 200 or response.status_code == 201:
+        # print('Token generated')
+        token = response.json()["data"]
+        print(token)
+        return token
+    else:
+        # print(response)
+        # print('Token generation failed')
+        return None
+
+# API 2: Get PRISM_TOC_response
+def get_PRISM_TOC_response(token, productId):
+    url = f"{constant.PRISM_TOC_API}/{productId}"
+    params = {
+        'productId': productId
+    }
+    headers = {
+        #'Content-Type':'application/json',
+        'Authorization': token,
+        'isDeeplink' : 'true'
+    }
+    response = requests.get(url=url, params=params, headers=headers)
+    if response.status_code == 200 or response.status_code == 201:
+        response_data = response.json()
+        print('passed')
+        return response_data
+    else:
+        print('not passed')
+        return None
+
+# API 3: Get PRISM_PAGE_MAPPING_response
+def get_PRISM_PAGE_MAPPING_response(token, productId):
+    url = f"{constant.PRISM_PAGE_MAPPING_API}/{productId}"
+    headers = {
+        #'Content-Type':'application/json',
+        'Authorization': token,
+        'isDeeplink' : 'true'
+    }
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200 or response.status_code == 201:
+        response_data = response.json()
+        print('passed')
+        return response_data
+    else:
+        print('not passed')
+        return None
+    
+#API 4: Post Course API
+# def post_Course_API(token, bookId):
+#     url = f"{constant.COURSE_API}"
+#     headers = {
+#         'Content-Type':'application/json',
+#         'Authorization': token,
+#     }
+#     body = {
+#         "bookIds": [
+#             bookId
+#         ],
+#         "productIds": [
+#             ""
+#         ]
+#     }
+#     response = requests.post(url=url, headers=headers, json=body)
+#     if response.status_code == 200 or response.status_code == 201:
+#         response_data = response.json()
+#         print('passed')
+#         return response_data
+#     else:
+#         print('not passed')
+#         return None
+
+#API 5: Resource API
+def get_Resource_API(token, productId):
+    url = f"{constant.RESOURCE_API}/{productId}&pageNo=1"
+    headers = {
+        'Authorization': token,
+        'isDeeplink': 'true'
+    }
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200 or response.status_code == 201:
+        response_data = response.json()
+        print('passed')
+        return response_data
+    else:
+        print('not passed')
+        return None
+    
+#API 6: CSG product API
+# def post_CSG_PRODUCT_API(token, bookId):
+#     url = f"{constant.CSG_PRODUCT_API}"
+#     headers = {
+#         'Content-Type' : 'application/json',
+#         'application-id' : 'marin',
+#         'Authorization': token
+#     }
+#     body = {
+#         "fieldsToReturn": [
+#           "productId",
+#           "product_id",
+#           "productSource",
+#           "title",
+#           "bookID",
+#           "book_id",
+#           "config",
+#           "globalBookID",
+#           "isbnList",
+#           "isbn13",
+#           "serverSideUuid",
+#           "description",
+#           "coverArt",
+#           "language",
+#           "editionTypeID",
+#           "authors",
+#           "author",
+#           "authorList",
+#           "pdfCoverArt",
+#           "cover_image_url",
+#           "navigationArt",
+#           "thumbnailArt",
+#           "thumbnail",
+#           "schema:images",
+#           "basePath",
+#           "uPdfUrl",
+#           "active",
+#           "layout",
+#           "bookServerUrl",
+#           "index_id",
+#           "productModelName",
+#           "version",
+#           "generalFeaturesTO.audioBook",
+#           "generalFeaturesTO.serverSideUuid",
+#           "generalFeaturesTO.serversideProcessed"
+#         ],
+#         "searchType": "FACET",
+#         "queryString": bookId,
+#         "responseSize": 200,
+#         "searchOnMultipleIndexes": 'true',
+#         "startIndex": 0
+#       }
+#     response = requests.post(url=url, headers=headers, json=body)
+#     if response.status_code == 200 or response.status_code == 201:
+#         response_data = response.json()
+#         print('passed')
+#         return response_data
+#     else:
+#         print('not passed')
+#         return None
+    
+
+
+
---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+ENVIRONMENT = "qa"  # type: ignore # prod/stg/qa
+
+QA_CONSTANTS = {
+    'TOKEN_URL'                 :   'https://login-stg.pearson.com/v1/piapi-int/login/webcredentials',
+    'PRISM_TOC_API'             :   'https://prism-qa.pearsoned.com/api/contenttoc/v1/assets?',
+    'PRISM_PAGE_MAPPING_API'    :   'https://prism-qa.pearsoned.com/api/contenttoc/v1/assets/page-mapping',
+    'CSG_PRODUCT_API'           :   'https://content-service-qa.stg-prsn.com/csg/api/v3/search',
+    'COURSE_API'                :   'https://stpaperapiqa.stg-prsn.com/etext/v2/courseboot/book/coursebookcatalogue',
+    'RESOURCE_API'              :   'https://prism-qa.pearsoned.com/api/contenttoc/v1/assets/hotspot?'
+}
