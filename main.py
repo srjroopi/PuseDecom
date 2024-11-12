de7ee153577103ceec3c5eaa56ddb0a65f78dc3f
Roopesh S <sroopesh6@gmail.com>
2024-11-12 11:21:52 +0530

    toc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# import constants as constant
-# from api_handler import get_PRISM_PAGE_MAPPING_response, get_Resource_API, get_token, get_PRISM_TOC_response
-# from save_response import save_JSON_response, save_XML_response, createDirectory
+import constants as constant
+from api_handler import get_PRISM_PAGE_MAPPING_response, get_Resource_API, post_token, get_PRISM_TOC_response
+from save_response import save_JSON_response, save_XML_response, createDirectory
 
 # # Prism Mapping
 # def process_PRISM_MAPPING_response(token, productId):
@@ -12,13 +12,13 @@
 #         print('PRISM Mapping Response not received')
 
 # # Prism TOC
-# def process_PRISM_TOC_response(token, productId):
-#     Toc_response_data = get_PRISM_TOC_response(token=token, productId=productId)
-#     if Toc_response_data:
-#         file_name = f'{productId}_PRISM_TOC.json'
-#         save_JSON_response(file_name, Toc_response_data)
-#     else:
-#         print('PRISM TOC Response not received')
+def process_PRISM_TOC_response(token, productId):
+    Toc_response_data = get_PRISM_TOC_response(token=token, productId=productId)
+    if Toc_response_data:
+        file_name = f'{productId}_PRISM_TOC.json'
+        save_JSON_response(file_name, Toc_response_data)
+    else:
+        print('PRISM TOC Response not received')
 
 # # Resource
 # def process_RESOURCE_response(token, productId):
@@ -29,37 +29,36 @@
 #     else:
 #         print('RESPONSE Response not received')
 
-# # Reading productId
-# def execute_request(productId,token):
-#     process_PRISM_TOC_response(token=token, productId=productId)
+# Reading productId
+def execute_request(productId,token):
+    process_PRISM_TOC_response(token=token, productId=productId)
 
 
-# def get_PRISM_TOC_(productIds,token):
-#     for productId in productIds:
-#         print(f'\nFetching for userId: {productId}')
-#         execute_request(productId=productId, token=token)
+def get_PRISM_TOC(productIds,token):
+    for productId in productIds:
+        print(f'\nFetching for userId: {productId}')
+        execute_request(productId=productId, token=token)
 
 # #  usage
 # username = constant.USERNAME
 # password = constant.PASSWORD
 
-# product_ids = []
-# with open(constant.PRODUCT_ID, 'r') as file:
-#     product_ids = [line.strip() for line in file.readlines()]
+product_ids = []
+with open(constant.PRODUCT_ID, 'r') as file:
+    product_ids = [line.strip() for line in file.readlines()]
 
 # # Create directory
 # createDirectory()
 # token = get_token(username, password)
 # print('token generated')
 
-# if token:
-#     get_PRISM_TOC_(productIds=product_ids, token=token)
-#     pass
-# else:
-#     print("Failed to get token")
-
-from api_handler import post_token
-
 print("posting")
 post_response = post_token()
 print('data', post_response)
+token = post_response
+
+if token:
+    get_PRISM_TOC(productIds=product_ids, token=token)
+    pass
+else:
+    print("Failed to get token")
