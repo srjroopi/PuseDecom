d1d088c997971878de1ef2e1b75eb0d548e54731
srjroopi <sroopesh6@gmail.com>
2024-11-12 09:37:36 +0530

    token generating

--- api_handler.py
+++ api_handler.py
@@ -1,12 +1,12 @@
 # api_handler.py
-import requests # type: ignore
+import requests
 import constants as constant
 
 # API 1: Get token
-def get_token():
-    url = constant.TOKEN_URL
+def post_token():
+    url = f"{constant.TOKEN_URL}"
     # data = {'userName': username, 'password': password}
-    # print(data)
+    print(url)
     headers = {
         'Content-Type':'application/x-www-form-urlencoded'
         }
@@ -17,53 +17,56 @@ def get_token():
         'password':'Test@123',
         'isMobile':'true'
     }
-    response = requests.post(url=url, headers=headers, json=body)
-    if response.status_code == 200 or response.status_code == 201:
-        # print('Token generated')
+    response = requests.post(url=url, headers=headers, data=body)
+    print(response)
+    if response.status_code in ( 200, 201 ):
+        print('Token generated')
         token = response.json()["data"]
-        print(token)
-        return token
+        # print('token',token)
+        access_token = token.get('access_token')
+        print(access_token)
+        return access_token
     else:
         # print(response)
         # print('Token generation failed')
         return None
 
 # API 2: Get PRISM_TOC_response
-def get_PRISM_TOC_response(token, productId):
-    url = f"{constant.PRISM_TOC_API}/{productId}"
-    params = {
-        'productId': productId
-    }
-    headers = {
-        #'Content-Type':'application/json',
-        'Authorization': token,
-        'isDeeplink' : 'true'
-    }
-    response = requests.get(url=url, params=params, headers=headers)
-    if response.status_code == 200 or response.status_code == 201:
-        response_data = response.json()
-        print('passed')
-        return response_data
-    else:
-        print('not passed')
-        return None
+# def get_PRISM_TOC_response(token, productId):
+#     url = f"{constant.PRISM_TOC_API}/{productId}"
+#     params = {
+#         'productId': productId
+#     }
+#     headers = {
+#         #'Content-Type':'application/json',
+#         'Authorization': token,
+#         'isDeeplink' : 'true'
+#     }
+#     response = requests.get(url=url, params=params, headers=headers)
+#     if response.status_code == 200 or response.status_code == 201:
+#         response_data = response.json()
+#         print('passed')
+#         return response_data
+#     else:
+#         print('not passed')
+#         return None
 
 # API 3: Get PRISM_PAGE_MAPPING_response
-def get_PRISM_PAGE_MAPPING_response(token, productId):
-    url = f"{constant.PRISM_PAGE_MAPPING_API}/{productId}"
-    headers = {
-        #'Content-Type':'application/json',
-        'Authorization': token,
-        'isDeeplink' : 'true'
-    }
-    response = requests.get(url=url, headers=headers)
-    if response.status_code == 200 or response.status_code == 201:
-        response_data = response.json()
-        print('passed')
-        return response_data
-    else:
-        print('not passed')
-        return None
+# def get_PRISM_PAGE_MAPPING_response(token, productId):
+#     url = f"{constant.PRISM_PAGE_MAPPING_API}/{productId}"
+#     headers = {
+#         #'Content-Type':'application/json',
+#         'Authorization': token,
+#         'isDeeplink' : 'true'
+#     }
+#     response = requests.get(url=url, headers=headers)
+#     if response.status_code == 200 or response.status_code == 201:
+#         response_data = response.json()
+#         print('passed')
+#         return response_data
+#     else:
+#         print('not passed')
+#         return None
     
 #API 4: Post Course API
 # def post_Course_API(token, bookId):
@@ -90,20 +93,20 @@ def get_PRISM_PAGE_MAPPING_response(token, productId):
 #         return None
 
 #API 5: Resource API
-def get_Resource_API(token, productId):
-    url = f"{constant.RESOURCE_API}/{productId}&pageNo=1"
-    headers = {
-        'Authorization': token,
-        'isDeeplink': 'true'
-    }
-    response = requests.get(url=url, headers=headers)
-    if response.status_code == 200 or response.status_code == 201:
-        response_data = response.json()
-        print('passed')
-        return response_data
-    else:
-        print('not passed')
-        return None
+# def get_Resource_API(token, productId):
+#     url = f"{constant.RESOURCE_API}/{productId}&pageNo=1"
+#     headers = {
+#         'Authorization': token,
+#         'isDeeplink': 'true'
+#     }
+#     response = requests.get(url=url, headers=headers)
+#     if response.status_code == 200 or response.status_code == 201:
+#         response_data = response.json()
+#         print('passed')
+#         return response_data
+#     else:
+#         print('not passed')
+#         return None
     
 #API 6: CSG product API
 # def post_CSG_PRODUCT_API(token, bookId):
