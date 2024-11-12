60af83a0cb821637c78bd22afd71cc7311d70b1b
srjroopi <sroopesh6@gmail.com>
2024-11-12 13:01:41 +0530

    filesadded

--- api_handler.py
+++ api_handler.py
@@ -24,7 +24,7 @@ def post_token():
         token = response.json()["data"]
         # print('token',token)
         access_token = token.get('access_token')
-        print(access_token)
+        # print(access_token)
         return access_token
     else:
         # print(response)
@@ -32,41 +32,46 @@ def post_token():
         return None
 
 # API 2: Get PRISM_TOC_response
-# def get_PRISM_TOC_response(token, productId):
-#     url = f"{constant.PRISM_TOC_API}/{productId}"
-#     params = {
-#         'productId': productId
-#     }
-#     headers = {
-#         #'Content-Type':'application/json',
-#         'Authorization': token,
-#         'isDeeplink' : 'true'
-#     }
-#     response = requests.get(url=url, params=params, headers=headers)
-#     if response.status_code == 200 or response.status_code == 201:
-#         response_data = response.json()
-#         print('passed')
-#         return response_data
-#     else:
-#         print('not passed')
-#         return None
+def get_PRISM_TOC_response(token, productId):
+    url = f"{constant.PRISM_TOC_API}productId={productId}"
+    # params = {
+    #     'productId': productId
+    # }
+    headers = {
+        #'Content-Type':'application/json',
+        'X-Authorization': token,
+        'isDeeplink' : 'true'
+    }
+    # print(url, token)
+    response = requests.get(url=url, headers=headers)
+    # print(response)
+    if response.status_code == 200 or response.status_code == 201:
+        response_data = response.json()
+        print('passed')
+        return response_data
+    else:
+        print('not passed')
+        return None
 
 # API 3: Get PRISM_PAGE_MAPPING_response
-# def get_PRISM_PAGE_MAPPING_response(token, productId):
-#     url = f"{constant.PRISM_PAGE_MAPPING_API}/{productId}"
-#     headers = {
-#         #'Content-Type':'application/json',
-#         'Authorization': token,
-#         'isDeeplink' : 'true'
-#     }
-#     response = requests.get(url=url, headers=headers)
-#     if response.status_code == 200 or response.status_code == 201:
-#         response_data = response.json()
-#         print('passed')
-#         return response_data
-#     else:
-#         print('not passed')
-#         return None
+def get_PRISM_PAGE_MAPPING_response(token, productId):
+    url = f"{constant.PRISM_PAGE_MAPPING_API}/{productId}"
+    headers = {
+        #'Content-Type':'application/json',
+        'X-Authorization': token,
+        'isDeeplink' : 'true'
+    }
+    # print(token, productId)
+    response = requests.get(url=url, headers=headers)
+    print(response)
+    if response.status_code == 200 or response.status_code == 201:
+        response_data = response.json()
+        # bookId = response_data.get("bookId")
+        print('passed')
+        return response_data
+    else:
+        print('not passed')
+        return None
     
 #API 4: Post Course API
 # def post_Course_API(token, bookId):
@@ -93,20 +98,20 @@ def post_token():
 #         return None
 
 #API 5: Resource API
-# def get_Resource_API(token, productId):
-#     url = f"{constant.RESOURCE_API}/{productId}&pageNo=1"
-#     headers = {
-#         'Authorization': token,
-#         'isDeeplink': 'true'
-#     }
-#     response = requests.get(url=url, headers=headers)
-#     if response.status_code == 200 or response.status_code == 201:
-#         response_data = response.json()
-#         print('passed')
-#         return response_data
-#     else:
-#         print('not passed')
-#         return None
+def get_Resource_API(token, productId):
+    url = f"{constant.RESOURCE_API}productId={productId}&pageNo=1"
+    headers = {
+        'X-Authorization': token,
+        'isDeeplink': 'true'
+    }
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200 or response.status_code == 201:
+        response_data = response.json()
+        print('passed')
+        return response_data
+    else:
+        print('not passed')
+        return None
     
 #API 6: CSG product API
 # def post_CSG_PRODUCT_API(token, bookId):
--- save_response.py
+++ save_response.py
@@ -1,9 +1,10 @@
+#save_response.py
 import json
 import os
 
 # Save response as JSON file
 def save_JSON_response(file_name, data):
-    file_path = os.path.join('Responses', file_name)
+    file_path = os.path.join('TOC', file_name)
     with open(file_path, "w") as file:
         json.dump(data, file, indent=4)
     # print(f"Response saved to {file_path}")
